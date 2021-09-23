b9a6ecf4caef288254db7bd314540128c81965a2
CptMiracle <56608216+CptMiracle@users.noreply.github.com>
2021-09-23 17:29:19 -0400

    t

--- test.cpp
+++ test.cpp
@@ -17,16 +17,66 @@ int main()
     cout.tie(0);
     vector<int> v;
     v.push_back(12);
-    v.push_back(13); 
+    v.push_back(13);
     v.insert(v.begin(), 14);
 
-    for (int i=0; i<v.size(); i++)  {
+    for (int i = 0; i < v.size(); i++)
+    {
         printf("%d", v[i]);
     }
 
-    for (const int &i: v) {
+    for (const int &i : v)
+    {
         printf("\n%d", i);
     }
-    return 0;
+    cout << endl;
 
+    queue<int> q;
+    q.push(10);
+    q.push(12);
+    while (!q.empty())
+    {
+        printf("\n%d", q.front());
+        q.pop();
+    }
+
+    pair<string, int> pair1 = {"hey", 123};
+
+    cout << pair1.first << endl;
+    pair1.second = 321;
+    cout << pair1.second << endl;
+
+    cout << endl;
+    tuple<string, string, int> tp2 = make_tuple("Hello", "world", 100);
+	string s1, s2; int x;
+	tie(s1, s2, x) = tp2;
+	cout << s1 << " " << s2 << " " << x << endl;
+
+
+    set<int> s;
+    s.insert(1);
+    s.insert(2);
+    s.insert(2);
+    for (int element: s) {
+        cout << element;
+    }
+    cout << endl;
+    map<int, int> m;
+    m[0] = 1;
+    m[1] = 2;
+    for (pair<int, int> i: m) {
+        printf("%d %d \n", i.first, i.second);
+    }
+
+    stack<int> stack;
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    while(!stack.empty()) {
+        cout << stack.top();
+        stack.pop();
+    }
+
+
+    return 0;
 }
