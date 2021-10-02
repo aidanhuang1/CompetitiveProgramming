3e5cf5bab8655617bb754c5ead115ecb143f7050
Aidan Huang <aidanhuangsiqi@gmail.com>
2021-10-02 15:18:22 -0400

    k

---
+++ C_Maximum_Median.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h> 
+#define INF 2e18
+#define MOD 1000000007
+using namespace std;
+typedef long long ll;
+ 
+int main() {
+cin.sync_with_stdio(0);
+cin.tie(0);
+
+int n, k;
+cin >> n >> k;
+int array[n];
+for (int i=0; i<n; i++) {
+    cin >> array[i];
+}
+sort(array, array+n);
+
+return 0;
+}
