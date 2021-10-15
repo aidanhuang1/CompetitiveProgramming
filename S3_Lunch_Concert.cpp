02001a5be8f19faf0269f28f255c9425c68ae9fd
CptMiracle <56608216+CptMiracle@users.noreply.github.com>
2021-10-15 09:01:31 -0400

    k

--- S3_Lunch_Concert.cpp
+++ S3_Lunch_Concert.cpp
@@ -6,6 +6,28 @@ using namespace std;
 typedef long long ll;
 
 vector<vector<ll>> friends;
+
+ll findtime(ll mid) {
+    ll temptime = 0;
+    for (int i=0; i<friends.size(); i++) {
+        ll timeforfriendtowalk = 0;
+        // if (mid>=friends[i][0]) { //if the friend is to the left of the concert
+        ll distance = abs(mid-friends[i][0])-friends[i][2]; //how far the friend has to walk
+        if (distance>0) { //friend is outside the hearing range, else we can ignore
+            // totaltime_leftside += distance*friends[i][1];
+            temptime += distance*friends[i][1];
+        }
+        }
+        // else if (mid<friends[i][0]) { //if the friend is to the right of the concert
+        //     ll distance = abs(mid-friends[i][0])-friends[i][2]; //how far the friend has to walk
+        //     if (distance>0) { //friend is outside the hearing range, else we can ignore
+        //         totaltime_rightside += distance*friends[i][1];
+        //     }
+        // }
+        return temptime;
+    }
+    
+
  
 int main() {
 cin.sync_with_stdio(0);
@@ -20,33 +42,21 @@ for (int i=0; i<n; i++) {
     }
 }
 
-ll left = 0, right = 2e9, besttime = 2e9;
+ll left = 0, right = 2e9, besttime = 2e9, currentpos=0;
 while(left<=right) {
     ll mid = (left+right)/2; //we test a position for the concert
-    ll totaltime_rightside = 0, totaltime_leftside = 0;
-    for (int i=0; i<n; i++) {
-        ll timeforfriendtowalk = 0;
-        if (mid>=friends[i][0]) { //if the friend is to the left of the concert
-            ll distance = abs(mid-friends[i][0])-friends[i][2]; //how far the friend has to walk
-            if (distance>0) { //friend is outside the hearing range, else we can ignore
-                totaltime_leftside += distance*friends[i][1];
-            }
-        }
-        else if (mid<friends[i][0]) { //if the friend is to the right of the concert
-            ll distance = abs(mid-friends[i][0])-friends[i][2]; //how far the friend has to walk
-            if (distance>0) { //friend is outside the hearing range, else we can ignore
-                totaltime_rightside += distance*friends[i][1];
-            }
-        }
+    currentpos = findtime(mid);
+    ll immediateleft = findtime(mid-1), immediateright = findtime(mid+1);
+
+    if (immediateleft >= currentpos && immediateright>= currentpos) {
+        break;
+    } else if (immediateleft>currentpos) {
+        left=mid+1;
+    } else if (immediateright>currentpos) {
+        right=mid-1;
     }
-    if (totaltime_rightside>totaltime_leftside) {
-        left = mid+1;
-    } else if (totaltime_rightside<=totaltime_leftside) {
-        right = mid-1;
-    }
-    besttime = min(besttime, totaltime_leftside+totaltime_rightside);
 }
-cout << besttime << endl;
+cout << currentpos << endl;
 
 return 0;
 }
