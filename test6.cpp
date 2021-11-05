f0ff93e160ef87b1145392d31ba0119a826902ee
CptMiracle <56608216+CptMiracle@users.noreply.github.com>
2021-11-05 08:53:41 -0400

    mm

---
+++ test6.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h> 
+#include <iostream>
+#define INF 2e18
+#define MOD 1000000007
+using namespace std;
+typedef long long ll;
+ 
+int main() {
+cin.sync_with_stdio(0);
+cin.tie(0);
+
+bool test[5];
+memset(test, false, sizeof(test));
+
+for (auto i: test) {
+    cout<<i<<endl;
+}
+cout<<boolalpha<<test[0]<<endl;
+ 
+return 0;
+}
