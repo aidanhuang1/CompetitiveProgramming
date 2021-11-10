f320a2d806dfcfa0a3d91cccae635bbdaaecc883
CptMiracle <56608216+CptMiracle@users.noreply.github.com>
2021-11-10 10:19:30 -0500

    ddsa

--- Bob_Escape_Maze.cpp
+++ Bob_Escape_Maze.cpp
@@ -11,37 +11,37 @@ int mr[] = {-1, 0, 1, 0, -2, -1, 1, 2, 2, 1, -1, -2};
 int mc[] = {0, 1, 0, -1, 1, 2,2,1,-1,-2,-2,-1};
 
 bool valid(pair<int, int> x) {
-    if (x.first<0 || x.first>=m || x.second<0 || x.second>=n || grid[x.first][x.second]=='#') {
-        return false;
+    if (x.first>=0 && x.first<m && x.second>=0 && x.second<n && grid[x.first][x.second]=='.') {
+        return true;
     }
-    return true;
+    return false;
 }
 
-void search(pair<int, int> start, pair<int, int> end) {
+int search(pair<int, int> start, pair<int, int> end) {
+    int moves = 0;
     queue<pair<int, int>> q;
-    int dist[m][n] = {0}; bool vis[m][n] = {false};
+    int dist[m][n]; bool vis[m][n];
+    memset(dist, 0, sizeof dist); memset(vis, false, sizeof vis);
     q.push(start);
     vis[start.first][start.second] = true;
     while(!q.empty()) {
         pair<int, int> cur = q.front(); q.pop();
-        
+        if (vis[end.first][end.second]) { return moves;}
         for (int i=0; i<12; i++) {
             int newr = cur.first+mr[i];
             int newc = cur.second+mc[i];
             if (valid({newr, newc}) && !vis[newr][newc]) {
-                dist[newr][newc] = dist[cur.first][cur.second]+1;
                 vis[newr][newc] = true;
                 q.push({newr, newc});
                 
             }
         }
-    if (vis[end.first][end.second]) {
-        cout<<dist[end.first][end.second]<<endl;
-        return;
+        moves++;
     }
-    }if (!vis[end.first][end.second]) {
-        cout<<-1<<endl;
+    if (vis[end.first][end.second]) {
+        return moves;
     } 
+    return -1;
     
 }
 
@@ -58,8 +58,8 @@ for (int i=0; i<m; i++) {
         cin>>grid[i][j];
     }
 }
-search({sr, sc}, {er, ec});
 
+search({sr, sc}, {er, ec});
  
 return 0;
 }
