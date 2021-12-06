0740082b1f238d57c4d937b46629d7c45b71feba
Aidan Huang <aidanhuangsiqi@gmail.com>
2021-12-05 20:43:23 -0500

    l

--- P1_Berry_Picking.cpp
+++ P1_Berry_Picking.cpp
@@ -4,39 +4,18 @@
 #define MOD 1000000007
 using namespace std;
 typedef long long ll;
-
-int n, k; vector<int> trees;
-int sol(int capacity) {
-    vector<int> baskets;
-    int ans=0;
-    for (auto t: trees) {
-        int remaining = t;
-        for (int i=0; i<remaining/capacity; i++) {
-            baskets.push_back(capacity);
-        }
-        if (remaining%capacity!=0) baskets.push_back(remaining%capacity);
-        
-    }
-    sort(baskets.begin(), baskets.end(), greater<int>());
-    
-    baskets.erase(baskets.begin(), baskets.begin()+((k/2)));
-    for (int i=0; i<2; i++) { ans+= baskets[i];}
-    return ans;
-}
  
 int main() {
 cin.sync_with_stdio(0);
 cin.tie(0);
+
+int n, k; 
 cin>>n>>k;
-int maxn=0, bestans=0;
-for (int i=0, x; i<n; i++) {
-    cin>>x; trees.push_back(x);
-    maxn = max(maxn, x);
+vector<int> b(n);
+for (int i=0; i<n; i++) {
+    cin>>b[i];
 }
-for (int i=1; i<=maxn; i++) {
-    bestans = max(bestans, sol(i));
-}
-cout<<bestans<<endl;
+
  
 return 0;
 }
--- S1_Big_Mattress_Tournament.cpp
+++ S1_Big_Mattress_Tournament.cpp
@@ -4,33 +4,17 @@
 #define MOD 1000000007
 using namespace std;
 typedef long long ll;
-
-bool check(int a, int b, int c) {
-    while(c>0) {
-        if (a<=0){
-            return false;
-        }
-        a--; c--;
-    }
-    b%=2;
-    if (b==1) {
-        b=0;
-        a-=2;
-    }
-    if (a<0 || a%2!=0) {
-        return false;
-    }
-    return true;
-}
  
 int main() {
 cin.sync_with_stdio(0);
 cin.tie(0);
 
-int n; cin>>n;
+int n;
+cin>>n;
 for (int i=0, a, b, c; i<n; i++) {
     cin>>a>>b>>c;
-    cout<<(check(a, b, c)?"YES":"NO")<<endl;
+    if (((a*1)+(b*2)+(c*3)%2!=0) || a<c || (b%2!=0 && a==0)) cout<<"NO"<<endl;
+    else cout<<"YES"<<endl;
 }
  
 return 0;
