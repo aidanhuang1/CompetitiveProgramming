7b3982ec86828905a7e535ba2fe1b440529f3fdc
Aidan Huang <aidanhuangsiqi@gmail.com>
2021-12-13 18:35:18 -0500

    d

--- P1_Sleepy_Cow_Herding.cpp
+++ P1_Sleepy_Cow_Herding.cpp
@@ -6,22 +6,23 @@ const int INF = 0x3F3F3F3F;
 const int MOD = 1e9+7;
 const int MM = 1e9+1;
 
-vector<int> cows(3);
+vector<ll> cows(3);
 
 int main() {
     cin.sync_with_stdio(0);
     cin.tie(0);
-    for (int i=0, x; i<3; i++) {
+    for (ll i=0; i<3; i++) {
         cin>>cows[i];
     }
     sort(cows.begin(), cows.end());
     if (cows[1]-cows[0]==1 && cows[2]-cows[1]==1) {
         cout<<0<<"\n";
-    } else if ()
-
-
-
-
+    } else if (cows[1]-cows[0]==2 || cows[2]-cows[1]==2) {
+        cout<<1<<"\n";
+    } else {
+        cout<<2<<"\n";
+    }
+    cout<<max(cows[1]-cows[0], cows[2]-cows[1])-1<<"\n";
 
     return 0;
 }
--- S1_World_of_StarCraft.cpp
+++ S1_World_of_StarCraft.cpp
@@ -6,17 +6,13 @@ const int INF = 0x3F3F3F3F;
 const int MOD = 1e9+7;
 const int MM = 1e6+1;
 
-
+int n, m, k;
 
 int main() {
 cin.sync_with_stdio(0);
 cin.tie(0);
-ll s[3];
-for (int i=0; i<3; i++) {
-    s[i]= 1;
-}
-cout<<s[0];
 
+cin>>n>>m>>k;
 
 return 0;
 }
