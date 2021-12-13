3692fbd376c2415e70dab09e779aeee8c748837a
CptMiracle <56608216+CptMiracle@users.noreply.github.com>
2021-12-13 17:37:08 -0500

    u

--- P1_Sleepy_Cow_Herding.cpp
+++ P1_Sleepy_Cow_Herding.cpp
@@ -4,35 +4,24 @@ typedef long long ll;
 typedef unsigned long long int ull;
 const int INF = 0x3F3F3F3F;
 const int MOD = 1e9+7;
-const int MM = 1e6+1;
+const int MM = 1e9+1;
 
-vector<int> cows, temp;
-
-int s() {
-    int i1 = abs(temp[1]-temp[0]), i2 = abs(temp[2]-temp[1]);
-    if (i1>=i2) {
-        return 0; //left
-    } else if (i1<i2) {
-        return 1; //right
-    }
-    return -1;
-}
+vector<int> cows(3);
 
 int main() {
-cin.sync_with_stdio(0);
-cin.tie(0);
-for (int i=0, a; i<3; i++) {
-    cin>>cows[i];
-}
-
-do {
-
-
-
-
-}
-while (cows[1]-cows[0]!=1 && cows[2]-cows[1]!=1);
+    cin.sync_with_stdio(0);
+    cin.tie(0);
+    for (int i=0, x; i<3; i++) {
+        cin>>cows[i];
+    }
+    sort(cows.begin(), cows.end());
+    if (cows[1]-cows[0]==1 && cows[2]-cows[1]==1) {
+        cout<<0<<"\n";
+    } else if ()
 
 
-return 0;
+
+
+
+    return 0;
 }
