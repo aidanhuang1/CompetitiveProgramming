2c7d66dbf3ab58ddbb75221d356b8537a47bd461
Aidan Huang <aidanhuangsiqi@gmail.com>
2021-12-14 20:51:27 -0500

    d

--- P2_Ebola_Outbreak.cpp
+++ P2_Ebola_Outbreak.cpp
@@ -4,16 +4,9 @@ typedef long long ll;
 typedef unsigned long long int ull;
 const int INF = 0x3F3F3F3F;
 const int MOD = 1e9+7;
-const int MM = 1e6+1;
+const int MM = 1e5+1;
 
-int n, m;
-
-int main() {
-cin.sync_with_stdio(0);
-cin.tie(0);
-
-cin>>n>>m;
-int parent[n+1], ranks[n+1];
+int n, m;int parent[MM], ranks[MM];
 int find(int x) {
     if (parent[x] != x) {
         parent[x] = find(x);
@@ -21,30 +14,47 @@ int find(int x) {
     return parent[x];
 }
 void uni(int a, int b) {
+    // cout<<a<<" "<<b<<endl;
     a = find(a), b=find(b);
+    
     if (a==b) return;
-    if (rank[a]>rank[b]) {
-        parent[b] = parent[a];
-    } else {
+    if (ranks[b]>ranks[a]) {
         parent[a] = b;
-        if (rank[a]=rank[b]) 
-        rank[b]++;
+    } else {
+        parent[b] = a;
+        if (ranks[b]==ranks[a]) 
+        ranks[a]++;
     }
+}
+int main() {
+cin.sync_with_stdio(0);
+cin.tie(0);
+
+cin>>n>>m;
+
+for (int i=1; i<=n; i++) {
+    parent[i]=i;ranks[i]=0;
 }
 
 for (int i=0, k; i<m; i++) {
     cin>>k; int prev;
     for (int j=0, x; j<k; j++) {
         cin>>x;
-        if (j>0) union(prev, x);
+        if (j>0) {
+            cout<<prev<<" "<<x;
+            uni(prev, x);
+        }
         prev = x;
     }
+    
 }
 vector<int> l;
-
 for (int i=1; i<=n; i++) {
-    if (find(i)==1) l.push_back(i);
+    if (find(i)==find(1)) l.push_back(i);
 }
+cout<<l.size()<<"\n";
+for (auto i: l) cout<<i<<" ";
+cout<<"\n";
 
 return 0;
 }
