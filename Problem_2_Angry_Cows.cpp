c69375f26c2f6cfea4a75dfa9b2cc05aa59176d3
CptMiracle <56608216+CptMiracle@users.noreply.github.com>
2021-12-19 10:24:57 -0500

    s

--- Problem_2_Angry_Cows.cpp
+++ Problem_2_Angry_Cows.cpp
@@ -1,42 +1,50 @@
 #include <bits/stdc++.h> 
 using namespace std;
 typedef long long ll;
-typedef unsigned long long int ull;
 const int INF = 0x3F3F3F3F;
 const int MOD = 1e9+7;
 const int MM = 1e5+1;
-int n;
-vector<int> v;
-void s(int pos) {
-    //right side
-    int range = 1, cnt = 1;
-    while (pos<n-1) {
-        if ((v[pos]+range)>=v[pos+1]) {//next cow is in range
-            cnt++;
-            range++;
-            pos++;
-        } else {
-            break;                              
-        }
-    }
-    cout<<cnt<<endl;
+
+int n; vector<int> cows;
+
+int check(int start, int direction) {
+	int radius = 1;
+	int prev = start;
+
+	while (true) {
+		int next = prev;
+		// Getting the furthest explosion index without exceeding the current radius
+		while (next + direction >= 0 && next + direction < n 
+			&& abs(cows[next + direction] - cows[prev]) <= radius) {
+			next += direction;
+		}
+
+		// We didn't find a new haybale, so the chain explosion is over
+		if (next == prev) {
+			break;
+		}
+
+		// Update our previous explosion and increment radius
+		prev = next;
+		radius++;
+	}
+	return abs(prev - start);
 }
 
+
 int main() {
-//freopen(, , stdin);
-//freopen(, , stdout);
+freopen("angry.in", "r", stdin);
+freopen("angry.out", "w", stdout);
 cin.sync_with_stdio(0);
 cin.tie(0);
 
-cin>>n;
-
+cin>>n; for (int i=0, a; i<n; i++) { cin>>a; cows.push_back(a);};
+sort(cows.begin(), cows.end());
+int best = 0;
 for (int i=0; i<n; i++) {
-    int a; cin>>a; v.push_back(a);
+    best = max(best, check(i, -1) + check(i, 1) + 1);
 }
-sort(v.begin(), v.end());
-for (int i=0; i<n; i++) {
-    s(i);
-}
+cout<<best<<"\n";
 
 return 0;
 }
