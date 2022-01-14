f05f5ff5230cc3c92d59ad450456f2e09e948b04
Aidan Huang <aidanhuangsiqi@gmail.com>
2022-01-13 22:26:20 -0500

    g

--- P3_Snowy_Slopes.cpp
+++ P3_Snowy_Slopes.cpp
@@ -12,10 +12,28 @@ cin.sync_with_stdio(0);
 cin.tie(0);
 
 int n, m; cin>>n>>m;
-vector<int> points;
+vector<pair<ll, ll>> points; vector<long double> slope;
 for (int i=0; i<n; i++) {
     ll x, y; cin>>x>>y;
-    points.pu
+    points.push_back({x, y});
+}
+for (int i=0; i<m; i++) {
+    ll k, d; cin>>k>>d;
+    slope.push_back(k*1.0/d);
+}
+unordered_set<long double> pointintercepted[n];
+for (int i=0; i<n; i++) {
+    for (int j=i+1; j<n; j++) {
+        long double gradient = (points[j].second-points[i].second)*1.0/(points[j].first-points[i].first);
+        for (auto s: slope) {
+            if (s==gradient) {
+                if (pointintercepted[i].size()==0 && pointintercepted[j].size()==0) {
+
+                }
+            }
+        }
+        
+    }
 }
 
 return 0;
